55d7f7ea53dfd291c71ffc3ea0e00de63fe764d7
lucas Meyble <lucasmeyble123@gmail.com>
2021-10-28 21:59:56 -0300

    recuperando infos do user no git hub + verificar se o usuario existe no db

--- node_heat/src/app.ts
+++ node_heat/src/app.ts
@@ -1,16 +1,21 @@
 import "dotenv/config";
-import express, { response } from "express";
+import express from "express";
+
+import { routes } from "./routes";
 
 const app = express();
 
+app.use(express.json());
+app.use(routes);
+
 app.get("/github", (request, response) => {
   response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
-})
+});
 
 app.get("/signin/callback", (request, response) => {
   const { code } = request.query;
 
   return response.json(code);
-})
+});
 
-app.listen(4000, () => console.log(`🚀 Server is runnig on PORT 4000`))
+app.listen(4000, () => console.log(`🚀 Server is runnig on PORT 4000`));
